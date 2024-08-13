dfc851c053fc2976f5c1c81965f53f6bd173f9d5
alemayehu.dev <alemayehu.dev@gmail.com>
2024-08-13 04:43:01 -0700

    v4.0.4

--- src/components/RegisterPageWrapper.js
+++ src/components/RegisterPageWrapper.js
@@ -1,37 +1,42 @@
-import React from 'react';
-import { Box, ImageListItem } from "@mui/material";
+import React from "react";
+import { Box, Grid, useMediaQuery } from "@mui/material";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
-const RegisterPageWrapper = ({ children}) => {
-    return (
-      <Box sx={{ height: "100vh", width: "100vw", display: "flex" }}>
-        {/* Left Half */}
-        <Box
-          sx={{
-            width: "50%",
-            height: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#171b36",
-            borderRight: "1px solid #ddd",
-          }}
-        >
-          <AutoStoriesIcon sx={{ fontSize: "300px", color: "#fff" }} />
-        </Box>
+const RegisterPageWrapper = ({ children }) => {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
-        {/* Right Half */}
-        <Box
-          sx={{
-            width: "50%",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          {children}
-        </Box>
-      </Box>
-    );
+  return (
+    <Box sx={{ height: "100vh", width: "100vw", display: "flex" }}>
+      <Grid container>
+        {!isMobile && (
+          <Grid item sm={0} md={6}>
+            <Box
+              sx={{
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#171b36",
+                borderRight: "1px solid #ddd",
+              }}
+            >
+              <AutoStoriesIcon sx={{ fontSize: "300px", color: "#fff" }} />
+            </Box>
+          </Grid>
+        )}
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            {children}
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default RegisterPageWrapper;
